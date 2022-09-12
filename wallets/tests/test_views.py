d38e5ebccd3d5a8515cadd80832fdfa9e5f11331
Tarcila Garcia <tarcilasg@gmail.com>
2022-09-12 18:35:46 -0300

    feat: views' test from Wallet

--- wallets/tests/test_views.py
+++ wallets/tests/test_views.py
@@ -1,9 +1,10 @@
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from users.models import User
+from wallets.models import Wallet
 
 
-class WalletViewTeste(APITestCase):
+class WalletViewTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = {
@@ -16,7 +17,18 @@ class WalletViewTeste(APITestCase):
             "cpf": "856.135.217-51",
         }
         cls.wallet = {
-            ...
+            "name": "testecoin",
+            "asset_ticket": "TST",
         }
-        cls.user_login = User.objects.create_user(**cls.user)
-        cls.user_login_token = Token.objects.get_or_create(user=cls.user_login)
+        cls.person = User.objects.create_user(**cls.user)
+        cls.person_token = Token.objects.get_or_create(user=cls.person)
+
+    def test_create_wallet(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.person_token[0]}")
+        response = self.client.post("/api/wallets/", self.wallet, format="json")
+        self.assertEqual(201, response.status_code)
+
+    def test_create_wallet_without_user(self):
+        response = self.client.post("/api/wallets/", self.wallet, format="json")
+        self.assertEqual(401, response.status_code)
+        self.assertIn("detail", response.data)
