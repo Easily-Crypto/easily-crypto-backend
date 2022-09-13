6c29c21947a61a7c0685f33ccd7637ef069d645e
Hyan Lopes <hyanlopess97@gmail.com>
2022-09-13 13:52:05 -0300

    fix: listing of transactions separated by wallets

--- transactions/views.py
+++ transactions/views.py
@@ -7,6 +7,7 @@ from wallets.models import Wallet
 
 from .api_data import DataCrypto
 from .models import Transaction
+from .permissions import IsOwnerWallet
 from .serializers import TransactionsSerializer
 
 
@@ -30,3 +31,17 @@ class ListCreateTransactionView(generics.ListCreateAPIView):
 
         except KeyError as err:
             return Response({"error": err.args[0]}, status=404)
+
+
+class ListTransactionView(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsOwnerWallet]
+
+    serializer_class = TransactionsSerializer
+
+    def get_queryset(self):
+
+        wallet = get_object_or_404(Wallet, id=self.kwargs["wallet_id"])
+
+        self.check_object_permissions(self.request, wallet)
+        return wallet.transactions
