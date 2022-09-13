686a557c18038104195337be4758d5d78da4ba28
Hyan Lopes <hyanlopess97@gmail.com>
2022-09-13 10:53:09 -0300

    feat: permissions for update sub_total wallet maded

--- wallets/serializers.py
+++ wallets/serializers.py
@@ -1,34 +1,16 @@
-from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework import serializers
-from transactions.api_data import DataCrypto
-from transactions.models import Transaction
 from transactions.serializers import TransactionsSerializer
-from users.serializers import UserSerializer
 
 from .models import Wallet
 
 
 class WalletSerializer(serializers.ModelSerializer):
     transactions = TransactionsSerializer(many=True, read_only=True)
-    sub_total = serializers.SerializerMethodField()
 
     class Meta:
         model = Wallet
         fields = "__all__"
         read_only_fields = ["user"]
-
-    def get_sub_total(self, obj):
-
-        wallet = get_object_or_404(Wallet, pk=obj.pk)
-        transactions = Transaction.objects.filter(wallets=obj.pk)
-
-        quantity = sum([float(item.quantity) for item in transactions])
-
-        crypto_quotation = float(
-            DataCrypto.get(crypto=wallet.asset_ticket)["price_actual"]
-        )
-        data = {"value": quantity * crypto_quotation}
-        return data["value"]
 
     def create(self, validated_data):
         wallet = Wallet.objects.create(**validated_data)
--- wallets/views.py
+++ wallets/views.py
@@ -8,6 +8,8 @@ from transactions.api_data import DataCrypto
 from wallets.models import Wallet
 from wallets.serializers import WalletSerializer
 
+from .permissions import IsOwnerWallet
+
 
 class WalletView(generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
@@ -26,20 +28,29 @@ class WalletView(generics.ListCreateAPIView):
 
 
 class SubTotalWalletView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsOwnerWallet]
+
     def get(self, req: Request, wallet_id) -> Response:
 
         wallet = get_object_or_404(Wallet, pk=wallet_id)
         serialized = WalletSerializer(wallet)
+        self.check_object_permissions(req, obj=serialized.data)
 
         quantity = sum(
-            [float(item["quantity"]) for item in serialized.data["transactions"]]
+            [
+                float(item["quantity"])
+                if item["exchange"] == "buy"
+                else -float(item["quantity"])
+                for item in serialized.data["transactions"]
+            ]
         )
 
         crypto_quotation = float(
             DataCrypto.get(crypto=wallet.asset_ticket)["price_actual"]
         )
 
-        data = {"sub_total": f"{quantity * crypto_quotation}"}
+        data = {"sub_total": quantity * crypto_quotation}
 
         sub_total_serialized = WalletSerializer(wallet, data=data, partial=True)
 
