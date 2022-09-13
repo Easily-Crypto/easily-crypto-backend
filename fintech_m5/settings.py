88adc12b03cd9fe65e3c5e33504629bbfed3c62e
Hyan Lopes <hyanlopess97@gmail.com>
2022-09-13 14:47:29 -0300

    heroku config
    Signed-off-by: Hyan Lopes <hyanlopess97@gmail.com>

--- fintech_m5/settings.py
+++ fintech_m5/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-*$3l+-i28)-w(v^ihicjxzq%vatq-(i94e!f-q*3jj154(y*8p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["easily-crypto-api.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["easily-crypto-api.herokuapp.com", "localhost"]
 
 
 # Application definition
