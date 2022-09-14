e0fd0efd0613cf09de8a8db24b8a32bc5769e537
Gabriel Schneider <gabrielmunizsch@gmail.com>
2022-09-14 18:52:36 -0300

    fix: external api error in invalid crypto name

--- transactions/api_data.py
+++ transactions/api_data.py
@@ -14,20 +14,23 @@ class DataCrypto:
     @classmethod
     def get(cls, crypto="BTC"):
         url = f'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol={crypto}&market=BRL&interval=1min&apikey={os.getenv("API_KEY")}'
-        try:
+        # try:
 
-            r = requests.get(url)
-            data = r.json()
+        r = requests.get(url)
+        data = r.json()
+        if len(data) > 1:
             price_actual = [item for item in data["Time Series Crypto (1min)"].items()][
                 0
             ]
-
+        
             obj_for_api = {
                 "information": data["Meta Data"],
                 "date_quotation": price_actual[0],
                 "price_actual": price_actual[1]["4. close"],
             }
-
             return obj_for_api
-        except:
-            raise AssetTicketNotExist(f"The {crypto} not exist in API database.")
+        else:
+            return False
+
+        # except data['information']:
+        #     raise AssetTicketNotExist(f"The {crypto} not exist in API database.")
